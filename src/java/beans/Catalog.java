609262df5cce498f5ce065d6c6ebf0f8538a32d0
Peti <antalpeti@users.noreply.github.com>
2015-07-11 23:09:55 +0200

    Add search feature to the film list.

--- src/java/beans/Catalog.java
+++ src/java/beans/Catalog.java
@@ -3,7 +3,6 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package beans;
 
 import java.util.ArrayList;
@@ -20,16 +19,32 @@ import pojos.Film;
 @ManagedBean
 @SessionScoped
 public class Catalog {
+
     private List<Film> fullFilmList;
     private List<Film> filteredFilmList;
+    private String actSearchParameter;
+    private List<String> searchParameterList;
+    private String searchText;
+
+    private static final String TITLE = "Title";
+    private static final String DIRECTOR = "Director";
+    private static final String TIME = "Time";
+    private static final String GENRE = "Genre";
+    private static final String YEAR = "Year";
 
     /**
      * Creates a new instance of Catalog
      */
     public Catalog() {
         queryFilmsFromDB();
+        searchParameterList = new ArrayList<String>();
+        searchParameterList.add(TITLE);
+        searchParameterList.add(DIRECTOR);
+        searchParameterList.add(TIME);
+        searchParameterList.add(GENRE);
+        searchParameterList.add(YEAR);
     }
-    
+
     private void queryFilmsFromDB() {
         Session session = hibernate.HibernateUtil.getSessionFactory().openSession();
         fullFilmList = session.createQuery("FROM Film").list();
@@ -40,11 +55,49 @@ public class Catalog {
         session.close();
     }
 
+    public void searchFilm() {
+        filteredFilmList.clear();
+        for (Film f : fullFilmList) {
+            if (((TITLE.equals(actSearchParameter) && f.getTitle().indexOf(searchText) != -1)
+                    || (DIRECTOR.equals(actSearchParameter) && f.getDirector().indexOf(searchText) != -1)
+                    || (TIME.equals(actSearchParameter) && f.getTime().indexOf(searchText) != -1)
+                    || (GENRE.equals(actSearchParameter) && f.getGenre().indexOf(searchText) != -1)
+                    || (YEAR.equals(actSearchParameter) && f.getYear() == Integer.parseInt(actSearchParameter)))
+                    || (searchText.isEmpty())) {
+                filteredFilmList.add(f);
+            }
+        }
+    }
+
     public List<Film> getFilteredFilmList() {
         return filteredFilmList;
     }
 
     public void setFilteredFilmList(List<Film> filteredFilmList) {
         this.filteredFilmList = filteredFilmList;
+    }
+
+    public String getActSearchParameter() {
+        return actSearchParameter;
+    }
+
+    public void setActSearchParameter(String actSearchParameter) {
+        this.actSearchParameter = actSearchParameter;
+    }
+
+    public List<String> getSearchParameterList() {
+        return searchParameterList;
+    }
+
+    public void setSearchParameterList(List<String> searchParameterList) {
+        this.searchParameterList = searchParameterList;
+    }
+
+    public String getSearchText() {
+        return searchText;
+    }
+
+    public void setSearchText(String searchText) {
+        this.searchText = searchText;
     }
 }
